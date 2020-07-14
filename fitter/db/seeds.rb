21e640fd6bafd6ee642d7f1958e4deb813d0c107
Felicia Grace Csolak <feliciacsolak@Felicias-MacBook-Pro.local>
2020-07-14 16:45:05 -0400

    attempting to seed sample data

--- fitter/db/seeds.rb
+++ fitter/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+
+@felicia=User.create!(username:'felicia', email: 'myemail@email.com', password: '123456', location: 'nyc', age: 34)
+
+@firstpost=Post.create!(title: 'cardio tuesday', content: 'I crushed my cycling session today!', exercise_type: 'Cycling', exercise_duration: '45min', calories: 300, user_id: @felicia)
+
+@secondpost=Post.create!(title: 'Lifting on Wed', content: 'Hardcore Kettlebell workout was so much fun.', exercise_type: 'Resistance', exercise_duration: '60min', calories: 500, user_id: @felicia)
+
+@thirdpost=Post.create!(title: 'Yoga on Friday', content: 'Nice and fluid vinyasa flow to end the week.', exercise_type: 'Yoga', exercise_duration: '60min', calories: 250, user_id: @felicia)
+
+@firstcomment=Comment.create!(text: "Nice work!", post_id: @firstpost )
+@secondcomment=Comment.create!(text: "Nice work!", post_id: @firstpost)
+@thirdcomment=Comment.create!(text: "Nice work!", post_id: @firstpost)
