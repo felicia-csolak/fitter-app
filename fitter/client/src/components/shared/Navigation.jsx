1afe60d33069e94d8736c58c00a88132a42f1023
Felicia Grace Csolak <feliciacsolak@Felicias-MacBook-Pro.local>
2020-07-16 17:10:42 -0400

    format changes

--- fitter/client/src/components/shared/Navigation.jsx
+++ fitter/client/src/components/shared/Navigation.jsx
@@ -5,32 +5,31 @@ export default class Navigation extends Component {
         userData: {
             username: '',
             password: ''
-          },
-          currentUser: null
-        }
-    
+        },
+        currentUser: null
+    }
+
     render() {
         return (
             <>
-              {this.props.currentUser ?
-            <h2>@{this.props.currentUser.username}</h2> :
-            <div className="login">
-              <form onSubmit={this.props.handleLogin}>
-                <div className="pair">
-                  <label htmlFor='username'>Username</label>
-                  <input name='username' type='text' value={this.props.userData.username} onChange={this.props.handleChange} />
-                </div>
+                {this.props.currentUser ?
+                    <h2>@{this.props.currentUser.username}</h2> :
+                    <div className="login-form">
+                        <form onSubmit={this.props.handleLogin}>
+                            <div className="username-form-value">
+                                <label htmlFor='username'>Username</label>
+                                <input name='username' type='text' value={this.props.userData.username} onChange={this.props.handleChange} />
+                            </div>
 
-                <div className="pair">
-                  <label htmlFor='password'>Password</label>
-                  <input name='password' type='password' value={this.props.userData.password} onChange={this.props.handleChange} />
-                </div>
+                            <div className="password-form-value">
+                                <label htmlFor='password'>Password</label>
+                                <input name='password' type='password' value={this.props.userData.password} onChange={this.props.handleChange} />
+                            </div>
 
-                <input type='submit' value="Login" />
-              </form>
-            </div>
-
-          } 
+                            <input type='submit' value="Login" />
+                        </form>
+                    </div>
+                }
             </>
         )
     }
